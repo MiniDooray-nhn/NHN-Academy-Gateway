515a4ebc70864702a3762a8d89c2493942400771
masiljangajji <hjk172262@gmail.com>
2024-01-28 13:15:22 +0900

    refactor: 기존 spec에 맞게 Account Controller 수정

--- src/main/java/com/nhnacademy/gateway/controller/AccountController.java
+++ src/main/java/com/nhnacademy/gateway/controller/AccountController.java
@@ -1,6 +1,7 @@
 package com.nhnacademy.gateway.controller;
 
 import com.nhnacademy.gateway.adapter.AccountAdaptor;
+import com.nhnacademy.gateway.dto.account.LoginRequest;
 import com.nhnacademy.gateway.dto.account.UserAuthDto;
 import com.nhnacademy.gateway.dto.account.UserModifyRequest;
 import com.nhnacademy.gateway.dto.account.UserRegisterRequest;
@@ -8,7 +9,12 @@ import com.nhnacademy.gateway.dto.account.UserResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 //@RequestMapping("/accounts")
@@ -17,7 +23,7 @@ public class AccountController {
     private final AccountAdaptor accountAdaptor;
 
     @GetMapping("/accounts/{id}")
-    public String getAccount(@PathVariable("id") String userId, Model model){
+    public String getAccount(@PathVariable("id") String userId, Model model) {
 
         UserResponse userResponse = accountAdaptor.getAccount(userId);
         model.addAttribute("account", userResponse);
@@ -26,28 +32,32 @@ public class AccountController {
     }
 
     @PostMapping("/accounts/{id}")
-    public String createAccount(@RequestBody UserRegisterRequest userRegisterRequest){
+    public String createAccount(@RequestBody UserRegisterRequest userRegisterRequest) {
         accountAdaptor.createAccount(userRegisterRequest);
         return "/account/loginForm";
     }
 
     @DeleteMapping("/accounts/{id}")
-    public String deleteAccount(@PathVariable("id") String userId){
+    public String deleteAccount(@PathVariable("id") String userId) {
         accountAdaptor.deleteAccount(userId);
         return "/account/loginForm";
     }
 
     @PutMapping("/accunts/{id}")
-    public String updateAccount(@RequestBody UserModifyRequest userModifyRequest){
+    public String updateAccount(@RequestBody UserModifyRequest userModifyRequest) {
         accountAdaptor.modifyAccount(userModifyRequest);
         return "/account/userModifyForm";
     }
 
-    public String matchesAccount (@RequestBody UserAuthDto userAuthDto){
-        if(accountAdaptor.matchesAccount(userAuthDto)){
+    public String matchesAccount(@RequestBody LoginRequest loginRequest) {
+
+        UserAuthDto userAuthDto = accountAdaptor.matchesAccount(loginRequest);
+
+        if (userAuthDto.isLoginUser()) {
             return "/account/home";
-        }else
+        } else {
             return "redirect:/account/loginForm";
+        }
     }
 
 }
